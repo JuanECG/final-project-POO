73aad6a1d22a3f8b3dbf76c88d36b3885c7b5dbd
Juan Esteban Castro <juancastro251@gmail.com>
2022-05-12 21:45:48 -0500

    se añade clase Acta

---
+++ model/acta.py
@@ -0,0 +1,13 @@
+
+class Acta:
+
+    def __init__(self,fecha,autor,nombre,tipo,director,jurado1,jurado2) -> None:
+        self.fecha = fecha
+        self.autor = autor
+        self.nombre = nombre
+        self.tipo = tipo
+        self.director = director
+        self.jurado1 = jurado1
+        self.jurado2 = jurado2
+
+
