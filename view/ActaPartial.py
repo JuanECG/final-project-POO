07ecf2766a15e5c517ebbac5821a772e99ba8155
Juan Esteban Castro <juancastro251@gmail.com>
2022-05-14 21:28:57 -0500

    se agrega funcionalidad para realizar la evaluacion de trabajos de grado

--- view/ActaPartial.py
+++ view/ActaPartial.py
@@ -1,6 +1,8 @@
+from msilib.schema import ControlCondition
+from random import random
 from model.acta import Acta
 
-""" Este archivo contiene las funcionalidades de la vista relacionada con la creación de actas"""
+""" Este archivo contiene las funcionalidades de la vista relacionada con las actas"""
 
 
 def crear_acta(st,controller):
@@ -14,13 +16,44 @@ def crear_acta(st,controller):
     acta.jurado1 = st.text_input("Nombre del primer jurado: ")
     acta.jurado2 = st.text_input("Nombre del segundo jurado: ")
 
-    enviado_btn = st.button("Guardar")
+    enviado_btn = st.button("Guardar")   
 
     if enviado_btn:
-        controller.agregar_evaluacion(acta)
+        controller.agregar_acta(acta)
         st.write("Evaluacion agregada exitosamente")
-    
     return controller
+
+def evaluar_acta(st,controller):
+    #TODO: mostrar actas y seleccionar una
+
+    #obtener nombres de actas
+    nombresActas=[]
+    for acta in controller.actas:
+        nombresActas.append(acta.nombre)    
+
+    nombreActa = st.selectbox('Seleccionar acta a evaluar: ',(nombresActas))
+
+    st.markdown("""---""")
+    
+    #obtener acta basado en el nombre
+    acta = controller.getActaNombre(nombreActa)
+
+    #listar criterios y obtener calificaciones
+    for criterio in acta.criterios:
+        st.write(str(criterio.id)+'. '+criterio.descripcion)
+        criterio.calificacion1  = st.number_input('Agregar calificacion 1:',key=criterio.id)
+        criterio.calificacion2  = st.number_input('Agregar calificacion 2:',key=criterio.id)
+
+    #agregar comentarios generales
+
+    acta.observaciones = st.text_area('Agrege las observaciones generales: ')  
+
+    enviado_btn = st.button("Guardar")   
+
+    if enviado_btn:
+        st.write("Evaluacion agregada exitosamente")
+        return controller
+
 
 def listar_actas(st, controller):
     """Itera los elementos de actas agregados y los muestra"""
@@ -34,3 +67,4 @@ def listar_actas(st, controller):
         st.write(acta.director)
         st.write(acta.jurado1)
         st.write(acta.jurado2)
+        st.write(acta.observaciones)
--- view/MainView.py
+++ view/MainView.py
@@ -3,8 +3,10 @@ from pydataxm.pydataxm import ReadDB
 from streamlit_option_menu import option_menu
 
 # from controller.MercadoController import MercadoController
+from controller.actaController import ActaController
 # from model.Consulta import Consulta
 # from model.Metrica import Metrica
+from view.ActaPartial import *
 from view.AboutPartial import *
 
 
@@ -17,22 +19,27 @@ class MainView:
             self.menu_actual = "About"
             # Inicialización de las variables necesarias
 
+            # Conexión con el controlador
+            self.controller = ActaController()
+
             st.session_state['main_view'] = self
         else:
             self.menu_actual = st.session_state.main_view.menu_actual
+            self.controller = st.session_state.main_view.controller
              # Carga de las variables necesarias
         self._inicialializar_layout()
 
     def _inicialializar_layout(self):
         # Set page title, icon, layout wide (more used space in central area) and sidebar initial state
-        st.set_page_config(page_title="Análisis mercado energía", page_icon='', layout="wide",
+        st.set_page_config(page_title="SITGRA", page_icon='', layout="wide",
                            initial_sidebar_state="expanded")
         # Defines the number of available columns del area principal
         self.col1, self.col2, self.col3 = st.columns([1, 1, 1])
 
         # Define lo que abrá en la barra de menu
         with st.sidebar:
-            self.menu_actual = option_menu("Menu", ["About", '[OtroMenu]Mi Menu'],
+            self.menu_actual = option_menu("Menu", ["About", "Crear Acta","Evaluar Acta",
+                                                    "Exportar Acta","Modificar Criterios","Ver Históricos"],
                                            icons=['house', 'gear'], menu_icon="cast", default_index=1)
 
     def ver_ejemplo(self):
@@ -48,8 +55,12 @@ class MainView:
             with welcome:
                 st.markdown(mostrar())
                 st.write("")
-        elif self.menu_actual == "[OtroMenu]Mi Menu":
-            self.ver_ejemplo()
+        elif self.menu_actual == "Crear Acta":
+            crear_acta(st,self.controller)
+        elif self.menu_actual == "Evaluar Acta":                        
+            evaluar_acta(st,self.controller)
+        elif self.menu_actual == "Ver Históricos":
+            listar_actas(st,self.controller)
 
 
 # Main call
